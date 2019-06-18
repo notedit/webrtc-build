260030405b8e78657465cedd01a24c1e3b1893ae
notedit <notedit@gmail.com>
2019-06-19 01:18:16 +0800

    fix ios lib

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 
 USER_WEBRTC_URL="https://github.com/notedit/webrtc-clone.git"
-OUT_DIR="$SRC_DIR/out_ios_libs"
+OUT_DIR="$SRC_DIR/ios_libs"
 PYTHON_ARGS="--output-dir $OUT_DIR --arch arm64 arm --extra-gn-args rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false rtc_enable_protobuf=false"
 
 mkdir $OUT_DIR
@@ -20,9 +20,9 @@ cd $SRC_DIR
 
 python tools_webrtc/ios/build_ios_libs.py $PYTHON_ARGS
 
-ls -al $SRC_DIR/out_ios_libs/
+ls -al $OUT_DIR/
 
-tar -zcvf $SRC_DIR/release.tar.gz $SRC_DIR/out_ios_libs/
+tar -zcvf $SRC_DIR/release.tar.gz  ./ios_libs/WebRTC.framework
 
 
 
