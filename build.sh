b57c82dc5e55ee6268cdb89490f84a8bf3a468bc
notedit <notedit@gmail.com>
2019-06-18 21:58:21 +0800

    fix arch

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 USER_WEBRTC_URL="https://github.com/notedit/webrtc-clone.git"
 OUT_DIR="$SRC_DIR/out_ios_libs"
-PYTHON_ARGS="--output-dir $OUT_DIR --arch arm64 armv7 --extra-gn-args rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false rtc_enable_protobuf=false"
+PYTHON_ARGS="--output-dir $OUT_DIR --arch arm64 arm --extra-gn-args rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false rtc_enable_protobuf=false"
 
 mkdir $OUT_DIR
 
