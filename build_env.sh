9766753511efb41ad62eb7228892c7cf2f2ad907
notedit <notedit@gmail.com>
2019-07-14 18:41:48 +0800

    build m76

--- build_env.sh
+++ build_env.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export BUILD_IOS="no"
+export BUILD_IOS="yes"
 export RELEASE_IOS="no"
 
 
-export BUILD_OSX="no"
+export BUILD_OSX="yes"
 export RELEASE_OSX="no"
 
 
 export BUILD_ANDROID="yes"
-export RELEASE_ANDROID="yes"
+export RELEASE_ANDROID="no"
 
 
-export BUILD_LINUX="no"
+export BUILD_LINUX="yes"
 export RELEASE_LINUX="no"
 
